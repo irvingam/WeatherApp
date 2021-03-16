ef5fdbbade6d4a43278a3ee894a9827b95e87872
Andrew-Irving <amirving94@gmail.com>
2021-03-16 12:43:14 -0400

    added functions for api, base, and results

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
 // Selectors
+const api = {
+    key: "dd27bad266e3ddbf9645ca1c6f57f7e5",
+    base: "https://api.openweathermap.org/data/2.5/"
+}
+
+const searchInput = document.querySelector('.search-box')
 
 // Events
+searchInput.addEventListener('keypress', setQuery)
 
 // Functions
+function setQuery(event) {
+    if (event.keyCode == 13) {
+        getResults(searchInput.value)
+        console.log(searchInput.value)
+    }
+}
+
+function getResults(query) {
+    fetch(`${api.base}weather?q=${query}&units=standard&APPID=${api.key}`)
+    .then(weather => {
+        return weather.json()
+    }).then(displayResults)
+}
+
+function displayResults(weather) {
+    console.log(weather)
+}
